443a9c19ecbd067584fa77d5dea2c6aaf8892e8d
Nathan Roe <nateroe63@gmail.com>
2023-01-30 00:08:44 -0500

    Remove debug printouts

--- Project1/main/main.ino
+++ Project1/main/main.ino
@@ -30,7 +30,7 @@ void loop()
 
 void inputLoop()
 {
-    Serial.println("Input Loop");
+    Serial.println("Please Provide Input");
     while(true)
     {
         char readByte = Serial.read();
@@ -38,48 +38,24 @@ void inputLoop()
         {
             morseManager->addLine(m, linePosition);
             linePosition = 0;
-            Serial.println("Sent Line");
             break;
         }
         else if (linePosition < MAX_LINE_LENGTH  && readByte != startChar)
         {
             message[linePosition++] = readByte;
-            Serial.print("Char added: ");
-            Serial.println(readByte);
         }
     }
 }
 
 void blinkLoop()
 {
-    Serial.println("Blink Loop");
     while(true)
-    {
-        Serial.println();
-        Serial.print("Current Time: ");
-        unsigned long currentTime = millis();
-        Serial.println(currentTime);
-        Serial.println(morseManager->getBlinkIdx());
-        
-        if (morseManager->ledChangeNeeded(currentTime))
+    {   
+        if (morseManager->ledChangeNeeded(millis()))
         {
             // if the LED is off turn it on and vice-versa
             changeLedState();
         }
-        Serial.print("Number of Chars: ");
-        Serial.println(morseManager->getCurrentLineLength());
-        Serial.print("Next Update: ");
-        Serial.println(morseManager->getNextUpdateTime());
-        Serial.print("Letter Index: ");
-        Serial.println(morseManager->getCurrentLineIndex());
-        Serial.print("Blink Index: ");
-        Serial.println(morseManager->getBlinkIdx());
-        Serial.print("Current Letter: ");
-        Serial.println(morseManager->getCurrentChar());
-        Serial.print("Current Duration: ");
-        Serial.println(morseManager->getCurrentDuration());
-        Serial.print("On Blink?: ");
-        Serial.println(morseManager->getOnBlink());
         
         if (morseManager->printComplete())
         {
@@ -93,12 +69,10 @@ void changeLedState()
         if (ledState == LOW)
         {
             ledState = HIGH;
-            Serial.println("Turning On");
         }
         else
         {
             ledState = LOW;
-            Serial.println("Turning Off");
         }
         digitalWrite(ledPin, ledState);
 }
