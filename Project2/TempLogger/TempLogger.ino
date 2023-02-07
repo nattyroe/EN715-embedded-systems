92f739fa3ebefc93c84612b922d20b02b162a719
Nathan Roe <nateroe63@gmail.com>
2023-02-06 20:31:21 -0500

    Add temp logging every 10s

--- Project2/TempLogger/TempLogger.ino
+++ Project2/TempLogger/TempLogger.ino
@@ -2,11 +2,16 @@
 #include <SoftwareSerial.h>
 
 const int thermistorPin = A0;
-int temperatureVal;
+unsigned long avgTempVal = 0;
+unsigned int tempCount = 0;
 
-const int logResetPin = 2;
-const int txPin = 1;
+unsigned long logInterval = 10000;
+unsigned long nextLogTime = millis() + logInterval;
+
 //const int rxPin = 0; // Currently unused
+const int txPin = 1;
+const int logResetPin = 2;
+//const int interruptPin = 3;
 
 void setup() {
     // put your setup code here, to run once:
@@ -15,13 +20,40 @@ void setup() {
     // Default state high, low to reset logging
     digitalWrite(logResetPin, HIGH);
 
+//    attachInterrupt(interruptPin, logAvgTemp, HIGH);
+
+    Serial.print("Time (s), Temp (F)\n");
+
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    temperatureVal = analogRead(thermistorPin);
-    Serial.println(temperatureVal);
+    unsigned long prevAvg = avgTempVal * tempCount;
+    ++tempCount;
+    avgTempVal = (prevAvg + analogRead(thermistorPin)) / tempCount;
 
+    if (millis() > nextLogTime){
+        //digitalWrite(interruptPin, HIGH);
+        logAvgTemp();
+    }
+}
+
+float tempValToFarenheit(unsigned int tempVal)
+{
+  return (float) tempVal;
+}
+
+void logAvgTemp()
+{
+    Serial.print(millis()/1000);
+    Serial.print(", ");
+    Serial.print(tempValToFarenheit(avgTempVal));
+    Serial.print(", ");
+    Serial.print(avgTempVal);
+    Serial.print("\n");
+    avgTempVal = 0;
+    tempCount = 0;
+    nextLogTime = millis() + logInterval;
+//    digitalWrite(interruptPin, LOW);
 }
 
 // Pull logger interrupt low
