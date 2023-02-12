5a180c880adffedb2ea1253389814ef6436662b3
Nathan Roe <nateroe63@gmail.com>
2023-02-12 12:39:35 -0500

    Update Cal temps/readings

--- Project2/TempLogger/TempLogger.ino
+++ Project2/TempLogger/TempLogger.ino
@@ -50,7 +50,7 @@ float tempValToFarenheit(unsigned int tempVal)
 {
     float measVoltage = ((1023.0 - tempVal)/ 1023.0) * inputVoltage;
     float measReistance = (measVoltage / inputVoltage) * resistanceUsed_ohm / (1 - (measVoltage / inputVoltage));
-    
+    Serial.println(measReistance);
     // Steinhart-Hart Equation
     float lnTemp = log(measReistance);
     float inverseTempK = SH_A + (SH_B * lnTemp) + (SH_C * lnTemp * lnTemp * lnTemp);
@@ -85,30 +85,25 @@ float farenheitToKelvin(float tFarenheit) {
 
 void solveSteinhartHart()
 {
-  // Calibration Temperatures
-//  float R1 = 7300;
-//  float T1 = farenheitToKelvin(170);
-  float R1 = 6900;
-  float T1 = farenheitToKelvin(98);
-//  float R2 = 13000;
-//  float T2 = farenheitToKelvin(67);
-  float R2 = 13000;
-  float T2 = farenheitToKelvin(67);
-//  float R3 = 15200;
-//  float T3 = farenheitToKelvin(32);
-  float R3 = 22200;
-  float T3 = farenheitToKelvin(40);
+    // Calibration Temperatures
+//    float R1 = 6900;
+    float R1 = 5500;
+    float T1 = farenheitToKelvin(98);
+    float R2 = 13000;
+    float T2 = farenheitToKelvin(67);
+    float R3 = 27000;
+    float T3 = farenheitToKelvin(40);
 
-  // Using Cal Temps, solve for SH Coefficients
-  float L1 = log(R1);
-  float L2 = log(R2);
-  float L3 = log(R3);
-  float Y1 = 1 / T1;
-  float Y2 = 1 / T2;
-  float Y3 = 1 / T3;
-  float gamma2 = (Y2 - Y1) / (L2 - L1);
-  float gamma3 = (Y3 - Y1) / (L3 - L1);
-  SH_C = ((gamma3 - gamma2) / (L3 - L2)) / (L1 + L2 + L3);
-  SH_B = gamma2 - (SH_C * ((L1*L1) + (L1*L2) + (L2*L2)));
-  SH_A = Y1 - ((SH_B + (L1*L1*SH_C))*L1);
+    // Using Cal Temps, solve for SH Coefficients
+    float L1 = log(R1);
+    float L2 = log(R2);
+    float L3 = log(R3);
+    float Y1 = 1 / T1;
+    float Y2 = 1 / T2;
+    float Y3 = 1 / T3;
+    float gamma2 = (Y2 - Y1) / (L2 - L1);
+    float gamma3 = (Y3 - Y1) / (L3 - L1);
+    SH_C = ((gamma3 - gamma2) / (L3 - L2)) / (L1 + L2 + L3);
+    SH_B = gamma2 - (SH_C * ((L1*L1) + (L1*L2) + (L2*L2)));
+    SH_A = Y1 - ((SH_B + (L1*L1*SH_C))*L1);
 }
