ae0f8c58abcc5ed48a14e07881f965dd1bfc37f7
Nathan Roe <nateroe63@gmail.com>
2023-02-26 16:20:33 -0500

    Add Project 3 documentation

--- Project3/RPMTracker/RPMTracker.ino
+++ Project3/RPMTracker/RPMTracker.ino
@@ -1,22 +1,29 @@
-
-// Pin Data
+// I/O Data
+// const int rxPin = 0; // Currently unused
+const int txPin = 1;
+const int logResetPin = 2;
 const int readPin = 3;
 
 // RPM Data
-unsigned long interruptCount = 0;
-unsigned long lastLogTime;
+volatile unsigned long interruptCount = 0;
+volatile unsigned long lastLogTime;
 const float NUM_BLADES = 2;
-const float MILLIS_TO_MIN = 60000;
+const float MILLIS_TO_MIN = 60000.;
 
 // Interrupt Data
-ISR(TIMER1_COMPA_vect) {
+ISR(TIMER1_COMPA_vect)
+{
     logRPM();
 }
 
 // Setup Serial bus transmissions and interrupts for
 // timer interrupt and digital pin interrupt
-void setup() {
-    // put your setup code here, to run once:
+void setup()
+{
+    // Default state high, low to reset logging
+    pinMode(logResetPin, OUTPUT);
+    digitalWrite(logResetPin, HIGH);
+
     Serial.begin(9600);
     pinMode(readPin, INPUT);
     Serial.print("Time (s), Rotation Speed (RPM)\n");
@@ -27,11 +34,11 @@ void setup() {
     // Set Timer 1 compare value for 1s (0x3D09 at 1024prescalar)
     OCR1A = 0x3D09;
     // Set Timer 1 to prescaler 1024
-    TCCR1B = (1<<CS12) | (1<<CS10);
+    TCCR1B = (1 << CS12) | (1 << CS10);
     // Set for comparison to OCR1A
-    TCCR1B = TCCR1B | (1<<WGM12);
+    TCCR1B = TCCR1B | (1 << WGM12);
     // Set Timer 1 to use compare match
-    TIMSK1 = (1<<OCIE1A);
+    TIMSK1 = (1 << OCIE1A);
 
     // Setup digital pin interrupt
     lastLogTime = millis();
@@ -41,18 +48,21 @@ void setup() {
 }
 
 // Loop doing nothing unless interrupts called
-void loop() {
+void loop()
+{
     // Empty, wait for interrupts
 }
 
 // Update propeller pass count on digital pin interrupt
-void updateInterruptCount() {
+void updateInterruptCount()
+{
     ++interruptCount;
 }
 
 // Log current RPM rate to serial bus
-void logRPM() {
-    Serial.print(millis()/1000.);
+void logRPM()
+{
+    Serial.print(millis() / 1000.);
     Serial.print(", ");
     Serial.print(calculateRPM());
     Serial.print("\n");
@@ -61,6 +71,16 @@ void logRPM() {
 }
 
 // Calculate RPM based on propellor pass count and delta time
-unsigned int calculateRPM() {
-    return ((interruptCount / NUM_BLADES) / (float) (millis() - lastLogTime)) * MILLIS_TO_MIN;
+unsigned int calculateRPM()
+{
+    return ((interruptCount / NUM_BLADES) / (float)(millis() - lastLogTime)) * MILLIS_TO_MIN;
+}
+
+// Reset Logger to create new log file
+void resetLogger()
+{
+    // Pull logger interrupt low
+    digitalWrite(logResetPin, LOW);
+    delay(10);
+    digitalWrite(logResetPin, HIGH);
 }
